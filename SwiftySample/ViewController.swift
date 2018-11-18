b481e5fc5ebfa7111e618dd6f75373185710d811
shtnkgm <nakagami.shota@gmail.com>
2018-11-18 21:36:47 +0900

    Initial Commit

---
+++ SwiftySample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftySample
+//
+//  Created by Shota Nakagami on 2018/11/18.
+//  Copyright © 2018 Shota Nakagami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
